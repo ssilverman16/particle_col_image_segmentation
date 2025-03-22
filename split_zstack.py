d7a65a33c9800afa75f876ea2311991de62a74c5
marshallyale <19430240+marshallyale@users.noreply.github.com>
2025-03-22 08:42:58 -0700

    update zstack parsing

--- split_zstack.py
+++ split_zstack.py
@@ -1,38 +1,68 @@
+'''
+1. Create folder with filename but no channel details (RFP, GFP, DAPI, CY5) and no zstack or mip (keep the number though)
+    2. 2 file structures: MIP and zstack which will be in last position before extension (.tif or .jpg)
+        Every zstack is just a tif, mip has both tif and jpg
+2. Move zstack/MIPs into the newly created folder
+3. Create folders in this folder, one for GFP and one for RFP
+
+
+'''
 import os
 
 import tifffile as tiff
 
 
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-def process_tif(input_file, output_folder, channel_indices):
+def get_clean_file_name(input_file):
+    base_name = input_file.split('.')[0]
+    if 'CY5_RFP_GFP_DAPI_' in base_name:
+        channels = "_CY5_RFP_GFP_DAPI"
+        clean_file_name = base_name.replace(channels, '').replace('_zstack', '').replace('_mip', '')
+    elif 'RFP_GFP_' in base_name:
+        channels = "_RFP_GFP"
+        clean_file_name = base_name.replace(channels, '').replace('_zstack', '').replace('_mip', '')
+    else:
+        channels = ""
+        clean_file_name = base_name
+    return (channels, clean_file_name)
+
+def create_channel_folder(destination, used_channels, channel_name):
+    clean_name = destination.replace(".tif", "").replace("_mip", "").replace(used_channels, "")
+    clean_name = clean_name + "_" + channel_name
+    create_folder(clean_name)
+    return clean_name
+
+def process_tif(input_file, channel_indices):
     channel_map = {0:"CY5", 1:"RFP", 2:"GFP", 3:"DAPI"}
-    file = tiff.TiffReader(input_file)
-    zstack = file.asarray()
     print('parsing file', input_file)
-
+    input_file_end = input_file.split('/')[-1].split('.')[0]
+    used_channels, clean_file_name = get_clean_file_name(input_file)
+    # new_tif_folder = f"{output_folder}/{clean_file_name}"
+    create_folder(clean_file_name)
+    # Move the original zstack file into the new folder
+    destination = os.path.join(clean_file_name, os.path.basename(input_file))
+    os.rename(input_file, destination)
+    if not input_file.endswith('.tif'):
+        return
+    file = tiff.TiffReader(destination)
+    zstack = file.asarray()
     for i, z_slice in enumerate(zstack):
         if z_slice.shape[0] != 4:
             channel_map = {0:"RFP", 1:"GFP"}
             channel_indices = [0,1]
 
         channel_names = [channel_map[channel_idx] for channel_idx in channel_indices]
-        selected_channels = z_slice[channel_indices]  # Extract channels 2 and 3
-        base_name = input_file.split('/')[-1].split('.')[0]
-
-        # conditional renaming
-        if 'CY5_RFP_GFP_DAPI_' in base_name:
-            clean_file_name = base_name.replace('CY5_RFP_GFP_DAPI_', '').replace('_zstack', '')
-        elif 'RFP_GFP_' in base_name:
-            clean_file_name = base_name.replace('RFP_GFP_', '').replace('_zstack', '')
-        else:
-            clean_file_name = base_name
-
+        selected_channels = z_slice[channel_indices]
         for idx, channel in enumerate(selected_channels):
             channel_name = channel_names[idx]
-            output_file = os.path.join(output_folder, f"{clean_file_name}_z{i}_{channel_name}.tif")
+            channel_folder = create_channel_folder(destination, used_channels, channel_name)
+            channel_file_name = input_file_end.replace(used_channels, "")
+            output_file = os.path.join(channel_folder, f"{channel_file_name}_z{i}_{channel_name}.tif")
             with tiff.TiffWriter(output_file, bigtiff=False) as tif:
                 tif.write(channel)
-    quit()
 
 def create_output_folder(file):
     folder_name = file.split(".")[0]
@@ -51,17 +81,17 @@ def process_folder(top_level_folder, channel_indices):
 
         # Process tif files in this directory
         for file in os.listdir(folder_path):
-            if file.lower().endswith('_zstack.tif'):
-                output_folder = create_output_folder(f"{folder_path}/{file}")
+            if file.lower().endswith('_zstack.tif') or file.lower().endswith('_mip.tif') or file.lower().endswith('_mip.jpg'):
+                # output_folder = create_output_folder(f"{folder_path}/{file}")
                 input_file = os.path.join(folder_path, file)
-                
+
                 # Process the file and save result
-                process_tif(input_file, output_folder, channel_indices)
+                process_tif(input_file, channel_indices)
 
 
 def main():
     channel_indices = [1, 2] # 1=RFP, 2=GFP
-    folder_name = '/Volumes/WD_Elements/3D05'
+    folder_name = '3D05_6B07'
     print("Processing folder: ", folder_name)
     process_folder(folder_name, channel_indices)
     print("Processing complete")
--- tiff_analysis.py
+++ tiff_analysis.py
@@ -1,99 +1,101 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
+
+'''
 Created on Wed Mar 12 19:00:25 2025
 
 @author: vercelli
-"""
+
+1. H5 file is how it will look
+2. Currently code is only extracting first zslice, want to extract all zslices and create pngs from them.
+    These pngs go under the respective channel folders created in split_zstack.py
+3. All tiffs and pngs will be in the same folder
+'''
 
 # Load libraries
-import tifffile
+import csv
 import os
+
 import h5py
 import matplotlib.pyplot as plt
 from matplotlib import colors
-
+from skimage.measure import label, regionprops
 
 # Visualize segmentation
-data_folder = '/Volumes/WD_Elements/3D05/24h'
 file_name = 'Tp_3D05_1_24h_60X_12_z_Simple Segmentation.h5'
-def print_structure(name, obj):
-    print(name, ":", "Group" if isinstance(obj, h5py.Group) else "Dataset")
+cmap = colors.ListedColormap(['#c0a0c0', '#1f607f', 'black']) # Cells, Diatoms, Background
 
 def find_datasets(name, obj):
     if isinstance(obj, h5py.Dataset):
         print(f"- {name}")
 
-with h5py.File(f'{data_folder}/{file_name}', "r") as h5f:
-    # Walk through the file and print its structure
-    h5f.visititems(find_datasets)
-    dataset_name = "exported_data"  # Change this to an actual dataset name
-    if dataset_name in h5f:
-        dataset = h5f[dataset_name]
-        print(f"Dataset: {dataset_name}")
-        print("Shape:", dataset.shape)
-        print("Datatype:", dataset.dtype)
-        print("First few values:", dataset[:5])
+def process_h5_file(file_path):
+    bounds = [.5, 1.5, 2.5, 3.5]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    with h5py.File(file_path, "r") as f:
+        a_group_key = next(iter(f.keys())) # retrieve first key in the HDF5 file
+        ds_arr = f[a_group_key][()]  # returns as a numpy array
 
-with h5py.File(os.path.join(data_folder, file_name), "r") as f:
-    a_group_key = list(f.keys())[0] # retrieve first key in the HDF5 file
-    # get the object type for a_group_key: usually group or dataset
-    print(type(f[a_group_key])) 
+    output_folder = file_path.replace('_segmentation.h5', '')
+    os.makedirs(output_folder, exist_ok=True) # Will probably need to change this to be more specific
+    for i, z_slice in enumerate(ds_arr):
+        print("Processing z-slice: ", i)
+        # Save basic image
+        plt.figure(figsize=(10, 10))
+        plt.imshow(z_slice, cmap=cmap, norm=norm, interpolation='None')
+        # plt.savefig(f"{output_folder}/{file_name}.png", bbox_inches='tight')
+        cell_positions, cell_clusters = get_cell_positions(z_slice)
+        plot_cell_positions(cell_positions, cell_clusters)
+        output_location = os.path.join(output_folder, f"{output_folder.split('/')[-1]}_cell_positions_{i}.png")
+        print("Saving to: ", output_location)
+        plt.savefig(output_location, bbox_inches='tight')
+        plt.close()
+    quit()
 
-    # If a_group_key is a dataset name, 
-    # this gets the dataset values and returns as a list
-    data = list(f[a_group_key])
-    
-    # preferred methods to get dataset values:
-    ds_obj = f[a_group_key]      # returns as a h5py dataset object
-    ds_arr = f[a_group_key][()]  # returns as a numpy array
+def get_cell_positions(z_slice):
+    label_im = label(z_slice) # converts ds_arr into a labeled image where each connected region gets a unique integer label
+    regions = regionprops(label_im) # finds connected regions in label_im, where each detected region becomes a regionprops
+    # object with properties (region.centroid, region.area, region.coords)
+    cell_pos = []
+    sizes = []
+    cell_clusters = []
+    for region in regions:
+        if get_type(region, z_slice) == 1: # check if the region is labeled as 1 (cells)
+            sizes.append(region.area)
+            if region.area >= 20 and region.area < 100:
+                cell_pos.append(region.centroid) # if true, stores the region's centroid (region.centroid) in cell_pos
+            if region.area >=100:
+                cell_clusters.append(region.centroid)
+    return cell_pos, cell_clusters
 
-# Crop image
-#ds_arr_cropped = ds_arr[0][500:1500, 500:1500]
-ds_arr = ds_arr[0]
-#print(ds_arr[0].shape)
+def get_type(region, data):
+    point = region.coords[0] # retrieves one coordinate (first pixel of the region)
+    return data[point[0], point[1]] # looks up the segmentation label at that pixel
 
-# Color labels
-cmap = colors.ListedColormap(['#c0a0c0', '#1f607f', 'black']) # Cells, Diatoms, Background
-bounds = [.5, 1.5, 2.5, 3.5]
-norm = colors.BoundaryNorm(bounds, cmap.N)
-plt.figure(figsize=(10, 10)) 
-plt.imshow(ds_arr, cmap=cmap, norm=norm, interpolation='None')
-plt.savefig(os.path.join(data_folder, f"{file_name}.png"), bbox_inches='tight')
+def plot_cell_positions(cell_pos, cell_clusters):
+    for pos in cell_pos:
+        plt.scatter(pos[1], pos[0], s=1, edgecolors='red')
+    for cluster in cell_clusters:
+        plt.scatter(cluster[1], cluster[0], s=3, edgecolors='blue')
 
+# plt.hist(sizes, bins=50, range=[0,200])
+# plt.savefig("area_distribution.png")
+def get_h5_files_recursively(folder_path):
+    h5_files = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.h5'):
+                h5_files.append(os.path.join(root, file))
+    return h5_files
 
-# Get x,y position of cells
-# from skimage.measure import label, regionprops, regionprops_table
-# import csv
-# def get_type(region, data):
-#     point = region.coords[0] # retrieves one coordinate (first pixel of the region)
-#     return data[point[0], point[1]] # looks up the segmentation label at that pixel
+def main():
+    folder_name = '3D05_example'
+    print("Processing folder: ", folder_name)
 
-# label_im = label(ds_arr) # converts ds_arr into a labeled image where each connected region gets a unique integer label
-# regions = regionprops(label_im) # finds connected regions in label_im, where each detected region becomes a regionprops 
-# # object with properties (region.centroid, region.area, region.coords)
+    h5_files = get_h5_files_recursively(folder_name)
+    for h5_file in h5_files:
+        print("Processing file: ", h5_file)
+        process_h5_file(h5_file)
+    print("Processing complete")
 
-# cell_pos = []
-# sizes = []
-# cell_clusters = []
-# for region in regions:
-#     if get_type(region, ds_arr) == 1: # check if the region is labeled as 1 (cells)
-#         sizes.append(region.area)
-#         if region.area >= 20 and region.area < 100:
-#             cell_pos.append(region.centroid) # if true, stores the region's centroid (region.centroid) in cell_pos
-#         if region.area >=100:
-#             cell_clusters.append(region.centroid)
-
-
-# with open("output.csv", "w", encoding='utf-8') as wf:
-#     writer =  csv.writer(wf)
-#     for pos in cell_pos:
-#         plt.scatter(pos[1], pos[0], s=1, edgecolors='red')
-#         writer.writerow(pos)
-# for cluster in cell_clusters:
-#     plt.scatter(cluster[1], cluster[0], s=3, edgecolors='blue')
-
-# plt.savefig('cell_pos_plot.png')
-# # plt.hist(sizes, bins=50, range=[0,200])
-# # plt.savefig("area_distribution.png")
-# # %%
+if __name__ == "__main__":
+    main()
